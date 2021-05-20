cce179605ab30be02699c5ae15a33c8b31d7db1f
jo-munoz <jo-munoz@hotmail.com>
2021-05-20 10:34:27 -0500

    Prueba terminada

--- GoSocket/Function1.cs
+++ GoSocket/Function1.cs
@@ -1,4 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -7,9 +14,33 @@ namespace GoSocket
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */30 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            String MainPath = Directory.GetCurrentDirectory();
+            XElement xmlRoot = XElement.Load(MainPath.Replace(@"bin\Debug\net461",@"Resource\File.xml"));
+            IEnumerable<XElement> lArea = xmlRoot.Descendants("area");
+
+            int AreaEmpleado = 0;
+            double salaryByArea = 0;
+
+            foreach (XElement area in lArea)
+            {
+                IEnumerable<XElement> lEmployee = area.Descendants("employee");
+
+                if (lEmployee.Count() > 2)
+                    AreaEmpleado += 1;
+
+                log.Info($"---------------------------------------------------------------------------");
+
+                foreach (XElement emp in lEmployee)
+                    salaryByArea += double.Parse(emp.Attribute("salary").Value);
+
+                log.Info(String.Format("{0}|{1}", area.Element("name").Value , salaryByArea));
+            }
+            
+            log.Info($"Nodos de tipo “<area>”: " + lArea.Count());
+            log.Info($"Nodos de tipo “<area>” que tiene mas de 2 Empleados: " + AreaEmpleado);
+            log.Info($"---------------------------------------------------------------------------");
         }
     }
 }
